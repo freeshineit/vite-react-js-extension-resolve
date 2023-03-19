48e67dd6fe05b142b6b50e726bf448b3ebcc26b6
ShineShao <xiaoshaoqq@gmail.com>
2023-03-19 23:04:28 +0800

    support swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 // import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // esbuild: {
-  //   loader: 'jsx'
-  // },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx'
-      }
-    }
-  },
-  server: {
-    // port: 3000, // 设置端口号
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-        // rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
@@ -34,11 +14,5 @@ export default defineConfig({
     minify: 'terser'
   },
   define: {},
-  plugins: [
-    react({
-      babel: {
-        plugins: ['@babel/plugin-transform-react-jsx']
-      }
-    })
-  ]
+  plugins: [react()]
 })
